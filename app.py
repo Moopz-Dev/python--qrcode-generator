813922efd028da01d48b7b2cabe2b27a636f4a2b
Moopz-Dev <pongzthor@gmail.com>
2022-04-10 21:57:02 +0700

    ✨ feat(app.py): generate qr command
    implements qr generation using pyqrcode

--- app.py
+++ app.py
@@ -1,5 +1,28 @@
 from tkinter import *
+import pyqrcode
+import png
+from PIL import ImageTk, Image
 
+
+# commands
+def generate_qrcode():
+    # retrieving inputs
+    link_name = name_entry.get()
+    link_url = link_entry.get()
+    # generating filename
+    file_name = link_name + ".png"
+    # creating qr
+    qr = pyqrcode.create(link_url)
+    # creating file
+    qr.png(file_name, scale=5)
+    # displaying QR
+    image = ImageTk.PhotoImage(Image.open(file_name))
+    image_label = Label(image=image)
+    image_label.image = image
+    canvas.create_window(200, 370, window=image_label)
+
+
+# initialization
 root = Tk()
 root.title("QR Code Generator")
 canvas = Canvas(root, width=400, height=500)
@@ -23,7 +46,7 @@ link_entry = Entry(root)
 canvas.create_window(200, 180, window=link_entry)
 
 # create QR Code Button
-button = Button(text="Generate QR Code")
+button = Button(text="Generate QR Code", command=generate_qrcode)
 canvas.create_window(200, 230, window=button)
 
 root.mainloop()
